3b30b7955ae8110e97694343139df75eeff3a476
rohitraikar565 <61474353+rohitraikar565@users.noreply.github.com>
2022-05-03 18:36:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "google" {
   credentials = "${file("inframod-training-09efb368bf98.json")}"
   project     = "inframod-training"
-  zone        = "us-central1"
+  zone        = "us-central1-a"
 }
-resource "google_compute_instance" "vmpolicy" {
+resource "google_compute_instance" "def" {
   name         = "test"
   machine_type = "e2-medium"
   #zone         = "us-central1-a"
