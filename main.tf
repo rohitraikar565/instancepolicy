9303fc8ef27a63faa30343cc205122d864125345
rohitraikar565 <61474353+rohitraikar565@users.noreply.github.com>
2022-05-03 18:13:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  credentials = "${file("inframod-training-09efb368bf98.json")}"
+  project     = "inframod-training"
+  zone        = "us-central1"
+}
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  count        = 2
+
+  tags = ["xp", "lp"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+}
